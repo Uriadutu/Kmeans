61833bfb4b8585ffd1fa733bd1402d71832a9d4e
Uriaaaa <126378661+Uriadutu@users.noreply.github.com>
2024-06-27 00:03:25 +0900

    Update gagal.js

--- gagal.js
+++ gagal.js
@@ -1,6 +1,6 @@
 // Fungsi untuk menghitung jarak Euclidean antara dua vektor
 function euclideanDistance(vector1, vector2) {
-  let sum = 0;
+  let sum = 0
   for (let i = 0; i < vector1.length; i++) {
     sum += Math.pow(vector1[i] - vector2[i], 2);
   }
